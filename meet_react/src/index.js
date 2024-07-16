f06280c923f1b6d284ad3b92fd0764798d324aaf
junyoung-choe <wjdghkswnsdu@naver.com>
2024-07-16 14:27:18 +0900

    feat: props and component study

--- meet_react/src/index.js
+++ meet_react/src/index.js
@@ -5,12 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Library />
-  </React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
 reportWebVitals();
