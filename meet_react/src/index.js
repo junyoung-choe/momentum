78932f2f47f6b08a8058cd1bc9b408d31ffa4c77
junyoung-choe <wjdghkswnsdu@naver.com>
2024-07-17 10:57:41 +0900

    feat: form and shared state study

--- meet_react/src/index.js
+++ meet_react/src/index.js
@@ -12,10 +12,12 @@ import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from "./chapter_11/SignUp";
+import Calculator from "./chapter_12/Calculator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AttendanceBook />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById("root")
 );
