c51e48fecd9e75058664b2dc43605c3756273e65
junyoung-choe <wjdghkswnsdu@naver.com>
2024-07-17 09:13:51 +0900

    feat: List and Keys study

--- meet_react/src/index.js
+++ meet_react/src/index.js
@@ -9,10 +9,13 @@ import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Accommodate />
+    <AttendanceBook />
   </React.StrictMode>,
   document.getElementById("root")
 );
